3503739734942dea654f75941cc13bcd20ab0923
代强 <daiqiang@agree.com.cn>
2018-01-30 16:48:04 +0800

    修改项目结构

---
+++ template/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <h1> This is App.vue !</h1>
+      <p> \{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            msg: "Hello Template !"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
